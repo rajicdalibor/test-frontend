4a4ae2ba4982c42f88df8dc51bd9b336965ab639
rajicdalibor <rajicdalibor@yahoo.com>
2023-10-25 15:03:16 +0200

    dfvdsfvdsf

--- react-app/src/counter.ts
+++ react-app/src/counter.ts
@@ -3,7 +3,6 @@ export function setupCounter(element: HTMLButtonElement) {
   const setCounter = (count: number) => {
     counter = count;
     element.innerHTML = `count is ${counter}`;
-
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
